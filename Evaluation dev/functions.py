4809a8f25c83d378b01f40409bbde227fee14dc9
Manal <147491097+manalajan1@users.noreply.github.com>
2024-12-06 20:23:57 +0100

    Update functions.py

--- Evaluation dev/functions.py
+++ Evaluation dev/functions.py
@@ -28,3 +28,21 @@ if __name__ == '__main__':
     m1 = Matrix(3, 4)  
     m2 = Matrix(3, 4)  
     print(m1 == m2)  
+    
+def mysort(mylist: [int]) -> [int]:
+    listcopy = mylist[:]
+
+    list_length = len(list_copy)
+
+    for i in range(list_length):
+        for j in range(list_length - i - 1):
+            if list_copy[j] > list_copy[j + 1]:
+                list_copy[j], list_copy[j + 1] = list_copy[j + 1], list_copy[j]
+
+    return list_copy
+
+if __name == '__main':
+    my_list = [17, 19, 8, 40, 1, 7, 22]
+    sorted_list = my_sort(my_list)
+    print("Liste triée :", sorted_list)
+
